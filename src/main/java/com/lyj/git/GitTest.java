bf4c3fddb25e057ada0ad04a81e5b5e102741aa1
Alex <Alex@lyj.com>
2022-10-17 22:19:41 +0800

    hot-fix commit1

--- src/main/java/com/lyj/git/GitTest.java
+++ src/main/java/com/lyj/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("Godbye Git!!!");
+        System.out.println("???Why do you come back？");
     }
 }
