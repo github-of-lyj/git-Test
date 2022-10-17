9a8efabff8360c18379f70726ef84c5c2faf2ad9
Alex <Alex@lyj.com>
2022-10-17 21:57:00 +0800

    second commit

--- src/main/java/com/lyj/git/GitTest.java
+++ src/main/java/com/lyj/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lyj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("Godbye Git!!!");
     }
 }
